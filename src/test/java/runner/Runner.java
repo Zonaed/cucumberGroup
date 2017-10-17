87158f4388bd79e160fdb98a4ca5fff14e5fa959
Zonaed <zbhuiyan13@gmail.com>
2017-10-17 15:03:27 -0400

    first commit

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,12 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\MD ZONAED BHUIYAN\\eclipse-workspace\\CucumberGroup\\src\\test\\java\\feature",glue="")
+public class Runner {
+
+}
